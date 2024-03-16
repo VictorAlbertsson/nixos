fcbee134c50288dac700a0849d18023c6f07f6a2
Victor Albertsson <github.301cs@silomails.com>
2024-03-16 01:56:29 +0100

    167 current  2024-03-16 01:56:13  23.11.20240305.880992d  6.1.80                          *

--- hosts/typewriter/default.nix
+++ hosts/typewriter/default.nix
@@ -29,6 +29,19 @@ in {
 
   hardware.cpu.amd.updateMicrocode = true;
   powerManagement.enable = true;
+  services.auto-cpufreq = {
+    enable = true;
+    settings = {
+      battery = {
+        governor = "powersave";
+        turbo = "never";
+      };
+      charger = {
+        governor = "performance";
+        turbo = "auto";
+      };
+    };
+  };
 
   hardware.enableAllFirmware = true;
   hardware.enableRedistributableFirmware = true;
