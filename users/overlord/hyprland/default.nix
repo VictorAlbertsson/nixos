57bacc28638f32ce76e411221e815f9a36ad015a
Victor Albertsson <github.301cs@silomails.com>
2024-05-24 18:12:11 +0200

    184 current  2024-05-24 18:12:05  23.11.20240522.4639777  6.1.91                          *

--- users/overlord/hyprland/default.nix
+++ users/overlord/hyprland/default.nix
@@ -22,7 +22,7 @@
               # Clear clipboard on startup
 	      "cliphist wipe"
       ];
-      monitor = ",hires,auto,2";
+      monitor = ",highres,auto,2";
       xwayland.force_zero_scaling = true;
       env = [
         "XCURSOR_SIZE,32"
