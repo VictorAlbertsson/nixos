592b0de0b395ab6759475c05586f90246175bd49
Victor Albertsson <github.301cs@silomails.com>
2024-08-02 15:55:34 +0200

    222 current  2024-08-02 15:55:27  24.05.20240730.cf05eea  6.6.43                          *

--- users/overlord/default.nix
+++ users/overlord/default.nix
@@ -94,53 +94,6 @@
     shellIntegration.enableBashIntegration = true;
   };
 
-  #programs.foot = {
-  #  enable = true;
-  #  server.enable = true;
-  #  settings = {
-  #    main = {
-  #      font = "M+1Code Nerd Font:size=12";
-	#      dpi-aware = "yes";
-  #    };
-  #    key-bindings = {
-  #      clipboard-copy = "Control+Shift+c XF86Copy";
-  #      clipboard-paste = "Control+Shift+v XF86Paste";
-  #      unicode-input = "Control+Shift+u";
-  #    };
-  #    colors = {
-  #      background = "${config.colorscheme.palette.base00}"; # "2e3440"
-  #      foreground = "${config.colorscheme.palette.base05}"; # "e5e9f0"
-
-  #      #* normal
-  #      regular0 = "${config.colorscheme.palette.base00}"; # "2e3440"
-  #      regular1 = "${config.colorscheme.palette.base0B}"; # "bf616a"
-  #      regular2 = "${config.colorscheme.palette.base0E}"; # "a3be8c"
-  #      regular3 = "${config.colorscheme.palette.base0D}"; # "ebcb8b"
-  #      regular4 = "${config.colorscheme.palette.base09}"; # "81a1c1"
-  #      regular5 = "${config.colorscheme.palette.base0F}"; # "b48ead"
-  #      regular6 = "${config.colorscheme.palette.base08}"; # "88c0d0"
-  #      regular7 = "${config.colorscheme.palette.base05}"; # "e5e9f0"
-
-  #      #* bright
-  #      bright0 = "${config.colorscheme.palette.base04}"; # "4c566a"
-  #      bright1 = "${config.colorscheme.palette.base0C}"; # "d08770"
-  #      bright2 = "${config.colorscheme.palette.base01}"; # "3b4252"
-  #      bright3 = "${config.colorscheme.palette.base02}"; # "434c5e"
-  #      bright4 = "${config.colorscheme.palette.base04}"; # "d8dee9"
-  #      bright5 = "${config.colorscheme.palette.base06}"; # "eceff4"
-  #      bright6 = "${config.colorscheme.palette.base0A}"; # "5e81ac"
-  #      bright7 = "${config.colorscheme.palette.base07}"; # "8fbcbb"
-
-  #      #* misc
-  #      selection-background = "${config.colorscheme.palette.base05}"; # "e5e9f0"
-  #      selection-foreground = "${config.colorscheme.palette.base00}"; # "2e3440"
-  #      urls = "${config.colorscheme.palette.base04}"; # "d8dee9"
-  #      jump-labels = "${config.colorscheme.palette.base00} ${config.colorscheme.palette.base0D}"; # "2e3440 ebcb8b"
-  #      scrollback-indicator = "${config.colorscheme.palette.base00} ${config.colorscheme.palette.base04}"; # "2e3440 d8dee9"
-  #    };
-  #  };
-  #};
-
   home.packages = with pkgs; [
     # Utilities
     # =========
